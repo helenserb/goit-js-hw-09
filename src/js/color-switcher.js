5b4b950a38c780b02b9217ddf31361a1e88a7dcd
helenserb <serbinenkoifbb@gmail.com>
2023-02-08 17:38:24 -0500

    completed task 1

---
+++ src/js/color-switcher.js
@@ -0,0 +1,29 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(){
+    startBtn.setAttribute('disabled', '');
+    stopBtn.disabled = false;
+    return timerID = setInterval(changeBcgColor, 1000)
+    }
+    
+
+function onStopBtnClick(){
+    startBtn.removeAttribute('disabled', '');
+    stopBtn.disabled = true;
+    clearInterval(timerID);
+
+
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function changeBcgColor(){
+ return body.style.backgroundColor = getRandomHexColor();
+}
+
