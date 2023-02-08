adff675e7273d90f82e99936feb45d275a90374b
helenserb <serbinenkoifbb@gmail.com>
2023-02-08 17:09:26 -0500

    c

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,1 @@
 
-const startBtn = document.querySelector('button[data-start]');
-console.log(startBtn);
-const stopBtn = document.querySelector('button[data-stop]');
-
-startBtn.disabled = false; 
-startBtn.addEventListener('click', onStartBtnClick); 
-stopBtn.addEventListener('click', onStopBtnClick);
-
-
-function onStartBtnClick() {
-    startBtn.setAttribute('disabled', '');
-}
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
